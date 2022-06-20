6eb5c1eab02b63643e6f371a81c5d858b7cbc856
Federico Barberon <66987826+FedericoBarberon@users.noreply.github.com>
2022-06-19 22:41:24 -0300

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const flexCol = document.querySelector(".flexCol");
+const flexRow = document.querySelector(".flexRow");
+
+flexCol.removeChild(flexCol.firstElementChild);
+flexRow.removeChild(flexRow.lastElementChild);
